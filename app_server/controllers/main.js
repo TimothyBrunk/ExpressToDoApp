560bc20df990d1f8dd4d2ede5676c2c0c81e039e
Timothy Brunk <Timothy.Brunk@gmail.com>
2016-05-26 08:48:47 -0600

    query is printing

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -10,12 +10,15 @@ var mysql = require('../models/mysql.js');
 
 //code for a get request
 module.exports.index = function(req, res){
+    var id = req.params.id;
     mysql.getConnection(function(err, con){
       con.query('Select * from expenses', function(err, rows){
           if(err) throw err;
           console.log("You are now connected");
 
-          res.render('index', {data:rows[0]});
+          res.render('index', {data:rows,
+                                Next:parseInt(id) + 1,
+                                Previous:parseInt(id) - 1});
       });
     });
 };
